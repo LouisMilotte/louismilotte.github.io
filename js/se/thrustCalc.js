2df328bac4c8c2e76715cf17ade68711ead641f6
Louis Milotte <louis.milotte@yahoo.com>
2018-08-11 10:13:36 -0400

    cleaning se tools

--- js/se/thrustCalc.js
+++ js/se/thrustCalc.js
@@ -16,9 +16,19 @@ $("#run-calculation").click(function () {
     var mass = parseFloat($("#ship_mass").val());
     var thrust = parseFloat($("#thrust").val());
     var accel = thrust / mass;
+    var netAccel = accel-gravity;
     if((accel - gravity) < 0){
-        alert("Flight not possible");
+        //alert("Flight not possible");
+        $("#thrust-calculator-output").addClass("text-warning");
+        $("#thrust-calculator-output").html("Flight not possible");
     }else{
-        alert("Flight possible, your acceleration is "+(accel-gravity)+" m/s/s");
+        //alert("Flight possible, your acceleration is "+(accel-gravity)+" m/s/s");
+        var seconds = 0;
+        var position = 0;
+        while(positon < 41000){
+            position+=netAccel;
+            seconds++;
+        }
+        $("#thrust-calculator-output").html("Flight is possible at "+netAccel+" m/s/s. You will achieve Earth-Like escape in "+seconds+" seconds.");
     }
 });
